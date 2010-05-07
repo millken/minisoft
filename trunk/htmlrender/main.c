7ff44104241772e60a5ff94a88799326583812b8
gamcat <gamcat@d6fae032-f293-11de-8f43-4547254af6c6>
2010-05-07 10:01:08 +0000

    难点来了,float

--- trunk/htmlrender/main.c
+++ trunk/htmlrender/main.c
@@ -7,6 +7,60 @@ enum {
 	M_SETROOT = WM_USER + 1001
 };
 
+typedef enum {
+	DISPLAY_BLOCK = 0, //块对象
+	DISPLAY_INLINE, //内联对象
+	DISPLAY_NONE, //隐藏对象
+
+	POSITION_STATIC = 0, //默认值。无特殊定位，对象遵循HTML定位规则
+	POSITION_ABSOLUTE, //绝对定位, 使用 left ， right ， top ， bottom 等属性相对于其最接近的
+					   //一个最有定位设置的父对象进行绝对定位。如果不存在这样的父对象，则依据 body 对象。
+	POSITION_FIXED, //屏幕位置
+	POSITION_RELATIVE, //相对定位, 在正常文档流中偏移位置
+
+	FLOAT_NONE = 0,
+	FLOAT_LEFT,
+	FLOAT_RIGHT,
+
+	OVERFLOW_VISIBLE = 0, //默认值。不剪切内容也不添加滚动条。
+	OVERFLOW_AUTO, //在必需时对象内容才会被裁切或显示滚动条
+	OVERFLOW_HIDDEN, //不显示超过对象尺寸的内容
+	OVERFLOW_SCROLL, //总是显示滚动条
+
+	FONTSTYLE_NORMAL = 0,
+	FONTSTYLE_ITALIC, //斜体
+	FONTSTYLE_OBLIQUE, //倾斜的字体
+
+	FONTWEIGHT_NORMAL = 400,
+	FONTWEIGHT_BOLD = 700,
+	FONTWEIGHT_BOLDER = 800,
+	FONTWEIGHT_LIGHTER = 100,
+
+	TEXTALIGN_LEFT = 0, //默认值。左对齐
+	TEXTALIGN_RIGHT,
+	TEXTALIGN_CENTER, //居中对齐
+	TEXTALIGN_JUSTIFY, //两端对齐
+
+	TEXTDEC_NONE = 0, //默认值。无装饰
+	TEXTDEC_UNDERLINE, //下划线
+	TEXTDEC_BLINK, //闪烁
+	TEXTDEC_OVERLINE, //上划线
+	TEXTDEC_THROUGH, //贯穿线
+
+	BORDERSTYLE_NONE = 0,
+	BORDERSTYLE_HIDDEN,
+	BORDERSTYLE_DOTTED,
+	BORDERSTYLE_DASHED,
+	BORDERSTYLE_SOLID,
+	BORDERSTYLE_DOUBLE,
+
+	VIS_INHERIT = 0,
+	VIS_VISIBLE,
+	VIS_COLLAPSE,
+	VIS_HIDDEN,
+
+} CSSCONST;
+
 /*
 
 function lex (dad) {
@@ -41,13 +95,81 @@ typedef struct {
 } Attr;
 
 typedef struct {
+	uint display; //none, block, inline
+	uint position;
+	uint _float;
+	uint visibility;
+	uint overflow_x;
+	uint overflow_y;
+
+	int left;
+	int top;
+	int right;
+	int bottom;
+	int height;
+	int min_height;
+	int max_height;
+	int width;
+	int min_width;
+	int max_width;
+
+	int margin_left;
+	int margin_top;
+	int margin_right;
+	int margin_bottom;
+	int padding_left;
+	int padding_top;
+	int padding_right;
+	int padding_bottom;
+
+	uint color;
+	uint background_color;
+
+	uint border_top_color;
+	int border_top_width;
+	uint border_top_style;
+	uint border_right_color;
+	int border_right_width;
+	uint border_right_style;
+	uint border_left_color;
+	int border_left_width;
+	uint border_left_style;
+	uint border_bottom_color;
+	int border_bottom_width;
+	uint border_bottom_style;
+
+	char* font_family; //const string
+	uint font_size;
+	uint font_style;
+	int font_weight;
+
+	uint text_align;
+	uint text_decoration;
+	int line_height;
+
+} CSS;
+
+typedef struct {
+	char* _class;
+	char* id;
+	bool checked;
+	bool disabled;
+	char* src;
+	char* href;
+	char* title;
+	char* value;
+} ATTR;
+
+typedef struct {
 	TagType type;
-	char* name;
-	wchar_t* wname;
-	size_t namelen; /* char count of wname */
+	char* name; /* utf-8 name */
+	wchar_t* wname; /* unicode name */
+	size_t namelen; /* char count of tagname */
 	Attr* attrs;
 	int an; /* count of attributes */
-	int linemaxh; /* max height of this line */
+	CSS css;
+	ATTR attr;
+	int linemaxh; /* for layout, max height of this line */
 } Tag;
 
 Tag* newtag (TagType type)
@@ -71,6 +193,197 @@ void deltag (Tag* tag)
 		sfree(tag->attrs);
 		free(tag);
 	}
+}
+
+void setstyle (Tag* tag, char* k, int klen, char* v, int vlen)
+{
+	char* val = sdumpn(v, vlen);
+
+	if (strncmp(k, "display", klen) == 0) {
+		if (strncmp(v, "none", vlen) == 0)
+			tag->css.display = DISPLAY_NONE;
+		else if (strncmp(v, "block", vlen) == 0)
+			tag->css.display = DISPLAY_BLOCK;
+		else if (strncmp(v, "inline", vlen) == 0)
+			tag->css.display = DISPLAY_INLINE;
+	}
+	else if (strncmp(k, "position", klen) == 0) {
+		if (strncmp(v, "static", vlen) == 0)
+			tag->css.position = POSITION_STATIC;
+		else if (strncmp(v, "absolute", vlen) == 0)
+			tag->css.position = POSITION_ABSOLUTE;
+		else if (strncmp(v, "fixed", vlen) == 0)
+			tag->css.position = POSITION_FIXED;
+		else if (strncmp(v, "relative", vlen) == 0)
+			tag->css.position = POSITION_RELATIVE;
+	}
+	else if (strncmp(k, "float", klen) == 0) {
+		if (strncmp(v, "none", vlen) == 0)
+			tag->css._float = FLOAT_NONE;
+		else if (strncmp(v, "left", vlen) == 0)
+			tag->css._float = FLOAT_LEFT;
+		else if (strncmp(v, "right", vlen) == 0)
+			tag->css._float = FLOAT_RIGHT;
+	}
+	else if (strncmp(k, "visibility", klen) == 0) {
+		if (strncmp(v, "inherit", vlen) == 0)
+			tag->css.visibility = VIS_INHERIT;
+		else if (strncmp(v, "visible", vlen) == 0)
+			tag->css.visibility = VIS_VISIBLE;
+		else if (strncmp(v, "collapse", vlen) == 0)
+			tag->css.visibility = VIS_COLLAPSE;
+		else if (strncmp(v, "hidden", vlen) == 0)
+			tag->css.visibility = VIS_HIDDEN;
+	}
+	else if (strncmp(k, "overflow", klen) == 0) {
+		if (strncmp(v, "visible", vlen) == 0) {
+			tag->css.overflow_x = OVERFLOW_VISIBLE;
+			tag->css.overflow_y = OVERFLOW_VISIBLE;
+		}
+		else if (strncmp(v, "auto", vlen) == 0) {
+			tag->css.overflow_x = OVERFLOW_AUTO;
+			tag->css.overflow_y = OVERFLOW_AUTO;
+		}
+		else if (strncmp(v, "hidden", vlen) == 0) {
+			tag->css.overflow_x = OVERFLOW_HIDDEN;
+			tag->css.overflow_y = OVERFLOW_HIDDEN;
+		}
+		else if (strncmp(v, "scroll", vlen) == 0) {
+			tag->css.overflow_x = OVERFLOW_SCROLL;
+			tag->css.overflow_y = OVERFLOW_SCROLL;
+		}
+	}
+	else if (strncmp(k, "overflow-x", klen) == 0) {
+		if (strncmp(v, "visible", vlen) == 0)
+			tag->css.overflow_x = OVERFLOW_VISIBLE;
+		else if (strncmp(v, "auto", vlen) == 0)
+			tag->css.overflow_x = OVERFLOW_AUTO;
+		else if (strncmp(v, "hidden", vlen) == 0)
+			tag->css.overflow_x = OVERFLOW_HIDDEN;
+		else if (strncmp(v, "scroll", vlen) == 0)
+			tag->css.overflow_x = OVERFLOW_SCROLL;
+	}
+	else if (strncmp(k, "overflow-y", klen) == 0) {
+		if (strncmp(v, "visible", vlen) == 0)
+			tag->css.overflow_y = OVERFLOW_VISIBLE;
+		else if (strncmp(v, "auto", vlen) == 0)
+			tag->css.overflow_y = OVERFLOW_AUTO;
+		else if (strncmp(v, "hidden", vlen) == 0)
+			tag->css.overflow_y = OVERFLOW_HIDDEN;
+		else if (strncmp(v, "scroll", vlen) == 0)
+			tag->css.overflow_y = OVERFLOW_SCROLL;
+	}
+	else if (strncmp(k, "left", klen) == 0) {
+		tag->css.left = atoi(val);
+	}
+	else if (strncmp(k, "top", klen) == 0) {
+		tag->css.top = atoi(val);
+	}
+	else if (strncmp(k, "right", klen) == 0) {
+		tag->css.right = atoi(val);
+	}
+	else if (strncmp(k, "bottom", klen) == 0) {
+		tag->css.bottom = atoi(val);
+	}
+	else if (strncmp(k, "height", klen) == 0) {
+		tag->css.height = atoi(val);
+	}
+	else if (strncmp(k, "min-height", klen) == 0) {
+		tag->css.min_height = atoi(val);
+	}
+	else if (strncmp(k, "max-height", klen) == 0) {
+		tag->css.max_height = atoi(val);
+	}
+	else if (strncmp(k, "width", klen) == 0) {
+		int w = 0;
+		int sn = sscanf (val, "%d%%", &w);
+		printf ("sn:%d, w:%d\n", sn, w);
+		tag->css.width = atoi(val);
+	}
+	else if (strncmp(k, "min-width", klen) == 0) {
+		tag->css.min_width = atoi(val);
+	}
+	else if (strncmp(k, "max-width", klen) == 0) {
+		tag->css.max_width = atoi(val);
+	}
+	else if (strncmp(k, "max-width", klen) == 0) {
+		tag->css.max_width = atoi(val);
+	}
+
+	sfree(val);
+}
+
+/* 解析属性中的css */
+void parseStyleAttr (Tag* tag, char* text, int len)
+{
+	char* s = text;
+	char* end = text+len;
+	char endchar = text[len];
+	char* k, *v;
+	int klen, vlen;
+
+	*end = '\0';
+
+	while (*text) {
+
+		while (*text && (isspace(*text) || *text==';'))
+			text++;
+		if (!*text) break;
+
+		k = s = text;
+		while (*s && *s!=':') s++;
+		if (!*s) break;
+		klen = s - text;
+
+		for (text = s + 1; *text && isspace(*text); text++);
+		if (!*text) break;
+
+		v = text;
+		for (s = text; *s && *s!=';'; s++);
+		vlen = s - text;
+
+		printf ("style=%s: %s\n", sdumpn(k, klen), sdumpn(v, vlen));
+		if (klen > 0 && vlen > 0)
+			setstyle (tag, k, klen, v, vlen);
+
+		text = s;
+	}
+
+	*end = endchar;
+}
+
+void setattr (Tag* tag, char* key, int klen, char* val, int vlen)
+{
+	char* v;
+	if (!tag || !key || !val) return;
+
+	v = sdumpn(val, vlen);
+
+	if (strncmp(key, "class", klen) == 0)
+		tag->attr._class = sdumpn(val, vlen);
+	else if (strncmp(key, "id", klen) == 0)
+		tag->attr.id = sdumpn(val, vlen);
+	else if (strncmp(key, "src", klen) == 0)
+		tag->attr.src = sdumpn(val, vlen);
+	else if (strncmp(key, "href", klen) == 0)
+		tag->attr.href = sdumpn(val, vlen);
+	else if (strncmp(key, "title", klen) == 0)
+		tag->attr.title = sdumpn(val, vlen);
+	else if (strncmp(key, "value", klen) == 0)
+		tag->attr.value = sdumpn(val, vlen);
+	else if (strncmp(key, "checked", klen) == 0)
+		tag->attr.checked = true;
+	else if (strncmp(key, "disabled", klen) == 0)
+		tag->attr.disabled = true;
+	// attribute was css
+	else if (strncmp(key, "width", klen) == 0)
+		tag->css.width = atoi(v);
+	else if (strncmp(key, "height", klen) == 0)
+		tag->css.height = atoi(v);
+	else if (strncmp(key, "style", klen) == 0)
+		parseStyleAttr (tag, val, vlen);
+
+	sfree(v);
 }
 
 void _parseElem (Tag* tag, char* text)
@@ -130,6 +443,7 @@ void _parseElem (Tag* tag, char* text)
 			a = &tag->attrs[tag->an++];
 			a->k = sdumpn(k, klen);
 			a->v = sdumpn(v, vlen);
+			setattr (tag, k, klen, v, vlen);
 		}
 
 		if (*s == '\0') return;
@@ -268,11 +582,14 @@ int layout_render (bool todraw, HDC hdc, glnode* dad, RECT* wrap, Tag* lh)
 	int left, top, width, height = 0;
 	int dadh = 0;
 
-	if (!dad) return 0;
+	if (!hdc || !dad) return 0;
 
 	top = wrap->top;
 	left = wrap->bottom; //起点
 	width = wrap->right;
+
+	if (width <= 0)
+		return top;
 
 	if (lh == NULL)
 		linehead = dad->child;
@@ -283,7 +600,7 @@ int layout_render (bool todraw, HDC hdc, glnode* dad, RECT* wrap, Tag* lh)
 		tag = (Tag*)node->data;
 
 		if (tag->type == TEXT) {
-			//printf ("%d\n", tag->len);
+			//printf ("taglen: %d\n", len);
 			int rowlen = 0;
 			ws = tag->wname;
 			len = tag->namelen;
@@ -306,7 +623,7 @@ int layout_render (bool todraw, HDC hdc, glnode* dad, RECT* wrap, Tag* lh)
 				// from the line start
 				if (left == wrap->left) {
 					linehead = tag;
-					printf ("text: %s, linemaxh: %d\n", tag->name, linehead->linemaxh);
+					//printf ("text: %s, linemaxh: %d\n", tag->name, linehead->linemaxh);
 					top += (left==wrap->left&&nextline) ? height : linehead->linemaxh;
 				}
 
@@ -352,7 +669,7 @@ int layout_render (bool todraw, HDC hdc, glnode* dad, RECT* wrap, Tag* lh)
 
 				if (left == wrap->left) {
 					linehead = tag;
-					printf ("img linemax: %d\n", linehead->linemaxh);
+					//printf ("img linemax: %d\n", linehead->linemaxh);
 					top += linehead->linemaxh;
 				}
 
@@ -407,27 +724,50 @@ LRESULT CALLBACK winproc (HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
 		return 0;
 
 		case WM_SIZE: {
-			HDC hdc = GetDC(hwnd);
+			HDC hdc = GetWindowDC(hwnd);
 			RECT rect;
 			GetClientRect (hwnd, &rect);
 			rect.right -= rect.left;
 			rect.bottom = rect.left;
 			layout_render (false, hdc, root, &rect, NULL);
 			ReleaseDC(hwnd, hdc);
-			return 0;
+			Sleep(1);
 		}
+		return 0;
+
+		case WM_ERASEBKGND:
+		return 0;
 
 		case WM_PAINT: {
 			PAINTSTRUCT ps;
 			HDC hdc = BeginPaint(hwnd, &ps);
 			RECT rect;
+			HDC hmem;
+			HBITMAP hbmp;
+			int w, h;
+
 			GetClientRect (hwnd, &rect);
+			hbmp = CreateCompatibleBitmap (hdc, rect.right, rect.bottom);
+			hmem = CreateCompatibleDC (hdc);
+			SelectObject (hmem, hbmp);
+
+			w = rect.right - rect.left;
+			h = rect.bottom - rect.top;
+
+			i32fillrect (hmem, &rect, 0x00ffffff);
+
 			rect.right -= rect.left;
 			rect.bottom = rect.left;
-			layout_render (true, hdc, root, &rect, NULL);
+			layout_render (true, hmem, root, &rect, NULL);
+
+			BitBlt (hdc, 0, 0, w, h, hmem, 0, 0, SRCCOPY);
+
+			DeleteObject(hbmp);
+			DeleteObject(hmem);
 			EndPaint(hwnd, &ps);
-			return 0;
+			Sleep(1);
 		}
+		return 0;
 	}
 
 	return DefWindowProc(hwnd, msg, wp, lp);
@@ -476,7 +816,7 @@ int WINAPI WinMain (HINSTANCE hithis, HINSTANCE hipre, PSTR param, int icmd)
 	char* buf, *s;
 	glnode* root;
 
-	buf = fdump ("index.htm");
+	buf = fdump ("test.htm");
 	root = glnew(newtag(ELEMENT));
 	s = lex (buf, root);
 
@@ -496,7 +836,6 @@ int WINAPI WinMain (HINSTANCE hithis, HINSTANCE hipre, PSTR param, int icmd)
 	i32loop();
 	}
 	///window
-
 
 	gldel(root, tagfreefunc);
 	sfree(buf);
